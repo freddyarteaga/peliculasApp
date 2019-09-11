50c52ff3cb0a60e7e2fb0d8121724a50b3c47e7a
Freddy <freddy.artea@gmail.com>
2019-09-10 20:07:39 -0500

    InitialCommit with created Components

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { SearchComponent } from './components/search/search.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PeliculaComponent,
+    SearchComponent,
+    TarjetasComponent,
+    LoadingComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HttpClientJsonpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+ 
+  private apikey = '410ea366735e5b1458ebf6187ec33801';
+  private urlMoviedb = 'https://api.themoviedb.org/3';
+ 
+  constructor(
+    private http: HttpClient
+  ) { }
+ 
+  getPopulares() {
+    const url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+    return this.http.jsonp(url, 'JSONP_CALLBACK');
+  }
+}
